95c1c5f43009bf82cacce3685efdd75cbd3016f9
Claus Witt <claus@wittnezz.dk>
2015-07-14 16:34:07 +0200

    removes environment var, since it is always run

--- start_local.sh
+++ start_local.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-YAPAF_DEV_SERVER=1 php -d variables_order=EGPCS -S localhost:8080 -t demo/public demo/public/index.php
+php -d variables_order=EGPCS -S localhost:8080 -t demo/public demo/public/index.php
 
